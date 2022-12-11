af4b871b39168f0c23993d56ce80d05ff2d40dd9
Shubham Mondal <shubhamank002@gmail.com>
2022-12-11 17:52:42 +0530

    Change to pool connection

--- config/db.js
+++ config/db.js
@@ -1,16 +1,27 @@
 const mysql = require("mysql");
 
-const mysqlConnection = mysql.createConnection({
+const db_config = {
+    connectionLimit: 100,
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DATABASE,
     multipleStatements: true,
-});
+};
 
-mysqlConnection.connect((err) => {
-    if (!err) console.log("Connection Established Successfully");
-    else console.log("Connection Failed!" + JSON.stringify(err, undefined, 2));
-});
+const pool = mysql.createPool(db_config);
 
-module.exports = mysqlConnection;
+// const mysqlConnection = mysql.createConnection(db_config);
+
+// mysqlConnection.connect((err, connection) => {
+//     if (err) {
+//         connection.release();
+//         console.log("Connection Failed!" + JSON.stringify(err, undefined, 2));
+//     } else {
+//         console.log("Connection Established Successfully");
+//     }
+// });
+
+// module.exports = mysqlConnection;
+
+module.exports = pool;
